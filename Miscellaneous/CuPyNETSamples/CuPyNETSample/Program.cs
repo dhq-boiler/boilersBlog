5c6287ebfc162af4651079849e00cdbf0f1f6db2
dhq_boiler <dhq_boiler@live.jp>
2024-02-15 23:43:12 +0900

    add CuPyNETSamples

---
+++ Miscellaneous/CuPyNETSamples/CuPyNETSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Cupy;
+using Python.Runtime;
+
+Runtime.PythonDLL = @"C:\Users\boiler\AppData\Local\Programs\Python\Python311\python311.dll";
+PythonEngine.Initialize();
+
+// before starting the measurement, let us call CuPy once to get the setup checks done. 
+cp.arange(1);
+
+var a1 = cp.arange(60000).reshape(300, 200);
+var a2 = cp.arange(80000).reshape(200, 400);
+
+var result = cp.matmul(a1, a2);
+
+Console.WriteLine(result.repr);
+
